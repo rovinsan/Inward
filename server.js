5309912ce5104748569ddf430c3c613514b15f23
Athiththan <kathirgamasagaran.ahtiththan@my.sliit.lk>
2017-03-23 19:50:20 +0530

    Server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -12,10 +13,10 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-app.set('views', __dirname + '/public/views');
+app.set('views', path.join(__dirname, '/public/views'));
 
 require('./app/routes/client.routes')(app);
 require('./app/routes/server.routes')(app);
