a9744175a6e20a0ada9f57ef3834118dfdb805f5
Athiththan <kathirgamasagaran.ahtiththan@my.sliit.lk>
2017-03-23 16:43:55 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const app   = express();
+const port  = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(express.static(__dirname + '/public'));
+
+app.use('*', (req, res, next) => {
+    res.send("Hello World");
+    next();
+});
+
+app.listen(port, function(err){
+    if(err){
+        console.error(err);
+        return;
+    }
+
+    console.log("Server running on port 3000");
+});
