30c5312b8e3a8291f1f7cf6efeddd12a82fcde39
Athiththan <kathirgamasagaran.ahtiththan@my.sliit.lk>
2017-03-24 10:15:32 +0530

    Boilerplate Update

---
+++ config/db.config.js
@@ -0,0 +1,5 @@
+// config/db.config
+
+module.exports = {
+    url: 'mongodb://athiththan:blackBenchers19@ds135680.mlab.com:35680/inward'
+}
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 app.set('views', path.join(__dirname, '/public/views'));
 
