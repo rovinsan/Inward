baa309b5f643160e335139bc81c7d8380a04ff71
Athiththan <kathirgamasagaran.ahtiththan@my.sliit.lk>
2017-03-26 22:28:04 +0530

    Code Quality Check

--- public/services/user.service.js
+++ public/services/user.service.js
@@ -6,7 +6,7 @@ angular.module('user.service', []).factory('User', ['$http', function($http) {
     return {
         get: function() {
             return $http.get('/api/users');
-        },
+        }
 
         // create: function(userData) {
         //     return $http.post('/api/users', userData);
