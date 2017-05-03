c405d0613ebfad10e718b79c378275308854d274
Rovinsan <srovinsan@outlook.com>
2017-05-03 16:36:14 +0530

    Doctor Model #1.2 update

--- app/models/doctor.model.js
+++ app/models/doctor.model.js
@@ -2,19 +2,19 @@
 
 const mongoose = require('mongoose');
 const DoctorSchema = mongoose.Schema({
-    did = String,
-    dname = String,
-    gender = String,
-    address = String,
+    did: String,
+    dname: String,
+    gender: String,
+    address: String,
     phone: {
         type: Number,
         validator: (v) => {
             return /d{10}/.test(v);
         }
     },
-    department = String,
-    designation = String
+    department: String,
+    designation: String
 
 
 });
-model.exports = mongoose.model('Doctor', doctorSchema);
+module.exports = mongoose.model('Doctor', DoctorSchema);
