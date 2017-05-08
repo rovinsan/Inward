92902c83a08a96f5c8ee4d9fc4aac2b80fbdc857
Athiththan <kathirgamasagaran.ahtiththan@my.sliit.lk>
2017-05-08 21:37:20 +0530

    test mongoURL

--- config/db.config.js
+++ config/db.config.js
@@ -2,5 +2,5 @@
 
 'use strict';
 module.exports = {
-    'url': 'mongodb://localhost:27017/test' //'mongodb://athiththan:blackBenchers19@ds135680.mlab.com:35680/inward'
+    'url': /*'mongodb://localhost:27017/test'*/ 'mongodb://athiththan:blackBenchers19@ds133221.mlab.com:33221/inwardtest' //'mongodb://athiththan:blackBenchers19@ds135680.mlab.com:35680/inward'
 };
